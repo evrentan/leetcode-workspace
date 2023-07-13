235d94de442447a8e6da822c85220427cf907984
Evren Tan <evrentan@gmail.com>
2023-07-13 10:29:59 +0300

    feat(n-th-tribonacci-number): Update 1137. N-th Tribonacci Number Solution
    Update 1137. N-th Tribonacci Number Solution
    
    - Add new solution with list and for
    
    - Add log execution time method

--- leetcode-75/n-th-tribonacci-number/src/NThTribonacciNumber.java
+++ leetcode-75/n-th-tribonacci-number/src/NThTribonacciNumber.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class NThTribonacciNumber {
@@ -11,17 +13,43 @@ public class NThTribonacciNumber {
         scanner.close();
 
         NThTribonacciNumber nThTribonacciNumber = new NThTribonacciNumber();
-        long start = System.currentTimeMillis();
+
+        long startTribonacci = System.currentTimeMillis();
         System.out.println(nThTribonacciNumber.tribonacci(n));
-        long end = System.currentTimeMillis();
-        System.out.println(String.format("Execution time is %d.%d seconds", (end - start) / 1000, (end - start) % 1000));
+        long endTribonacci = System.currentTimeMillis();
+        nThTribonacciNumber.logMethodExecutionTime("tribonacci", startTribonacci, endTribonacci);
+
+        long startTribonacciRecursion = System.currentTimeMillis();
+        System.out.println(nThTribonacciNumber.tribonacciRecursion(n));
+        long endTribonacciRecursion = System.currentTimeMillis();
+        nThTribonacciNumber.logMethodExecutionTime("tribonacciRecursion", startTribonacciRecursion, endTribonacciRecursion);
     }
 
-    public int tribonacci(int n) {
+    private int tribonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+
+        List<Integer> tribonacciList = new ArrayList<>();
+        tribonacciList.add(0);
+        tribonacciList.add(1);
+        tribonacciList.add(1);
+
+        for (int i = 3; i <= n; i++) {
+            tribonacciList.add(tribonacciList.get(i - 1) + tribonacciList.get(i - 2) + tribonacciList.get(i - 3));
+        }
+
+        return tribonacciList.get(n);
+    }
+
+    private int tribonacciRecursion(int n) {
 
         if (n == 0) return 0;
         if (n == 1 || n == 2) return 1;
 
-        return this.tribonacci(n - 3) + this.tribonacci(n - 2) + this.tribonacci(n - 1);
+        return this.tribonacciRecursion(n - 3) + this.tribonacciRecursion(n - 2) + this.tribonacciRecursion(n - 1);
+    }
+
+    private void logMethodExecutionTime(String methodName, long startTribonacciRecursion, long endTribonacciRecursion) {
+        System.out.println(String.format("%s method execution time is %d.%d seconds", methodName, (endTribonacciRecursion - startTribonacciRecursion) / 1000, (endTribonacciRecursion - startTribonacciRecursion) % 1000));
     }
 }
