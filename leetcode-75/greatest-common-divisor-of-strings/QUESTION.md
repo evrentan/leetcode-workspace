c8ef2749c1b7ccc1d38e3a5242221f33fe3bb615
Evren Tan <evrentan@gmail.com>
2023-07-24 10:58:25 +0300

    feat(greatest-common-divisor-of-strings): Add 1071. Greatest Common Divisor of Strings Solution
    Add 1071. Greatest Common Divisor of Strings Solution

---
+++ leetcode-75/greatest-common-divisor-of-strings/QUESTION.md
@@ -0,0 +1,30 @@
+# [1071. Greatest Common Divisor of Strings][question-link]
+
+For two strings s and t, we say "t divides s" if and only if s = t + ... + t (i.e., t is concatenated with itself one or more times).
+
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+### Example 1:
+```text
+Input: str1 = "ABCABC", str2 = "ABC"
+Output: "ABC"
+```
+
+### Example 2:
+```text
+Input: str1 = "ABABAB", str2 = "ABAB"
+Output: "AB"
+```
+
+### Example 3:
+```text
+Input: str1 = "LEET", str2 = "CODE"
+Output: ""
+```
+
+### Constraints:
+
+* 1 <= str1.length, str2.length <= 1000
+* str1 and str2 consist of English uppercase letters.
+
+[question-link]: https://leetcode.com/problems/greatest-common-divisor-of-strings/?envType=study-plan-v2&envId=leetcode-75
---
+++ leetcode-75/greatest-common-divisor-of-strings/src/GreatestCommonDivisorOfStrings.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class GreatestCommonDivisorOfStrings {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first string:");
+        String str1 = scanner.nextLine();
+        System.out.println("Enter the second string:");
+        String str2 = scanner.nextLine();
+
+        scanner.close();
+
+        GreatestCommonDivisorOfStrings greatestCommonDivisorOfStrings = new GreatestCommonDivisorOfStrings();
+        System.out.println(greatestCommonDivisorOfStrings.gcdOfStrings(str1, str2));
+    }
+
+    public String gcdOfStrings(String str1, String str2) {
+
+        if (str1.length() < str2.length())
+            return gcdOfStrings(str2, str1);
+
+        if (str2.isEmpty())
+            return str1;
+
+        if (str1.startsWith(str2))
+            return gcdOfStrings(str1.substring(str2.length()), str2);
+
+        return "";
+    }
+}
